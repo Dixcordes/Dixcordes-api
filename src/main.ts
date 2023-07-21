1b8737122899b518f1fdf7620398bfde93922c09
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2023-07-21 14:27:54 +0200

    feat: add the websocket to the main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+
+  app.useWebSocketAdapter(new IoAdapter(app));
+
+  // Activer CORS si nécessaire
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
