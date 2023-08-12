e29773cb963d1903ce0bde72316fa6e534ed924a
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2023-08-12 02:12:27 +0200

    feat: add the event serverMessage

--- src/websocket/gateway.module.ts
+++ src/websocket/gateway.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { MyGateway } from './gateway';
 import { UsersService } from 'src/users/users.service';
 import { UsersModule } from 'src/users/users.module';
+import { ServersModule } from 'src/servers/servers.module';
+import { ServersService } from 'src/servers/servers.service';
 
 @Module({
-  imports: [UsersModule],
-  providers: [MyGateway, UsersService],
+  imports: [UsersModule, ServersModule],
+  providers: [MyGateway, UsersService, ServersService],
 })
 export class GatewayModule {}
