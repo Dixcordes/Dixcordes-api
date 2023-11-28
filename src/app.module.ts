97c9f449e4a3e8bda2154a64cd3096a555ac1533
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2023-11-28 17:31:00 +0100

    Add resolveJsonModule and sequelize-cli dependencies, update config.json and .sequelizerc

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,16 +10,14 @@ import { ServersModule } from './servers/servers.module';
 import { AuthGuard } from './auth/auth.guard';
 import { ServerUser } from './server-user/server-user.model';
 import { Server } from './servers/server.model';
+import * as config from '../config/config.json';
 
+const DbDevConfig = config.development;
 @Module({
   imports: [
     SequelizeModule.forRoot({
+      ...DbDevConfig,
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'cordes',
       models: [User, Server, ServerUser],
       autoLoadModels: true,
       synchronize: true,
