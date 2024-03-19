d98970b126c5a82a1a2da834cbf3f96fb520bc5f
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2024-03-19 22:21:05 +0100

    fix: remove unused import in auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,4 @@
-import {
-  HttpException,
-  HttpStatus,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from 'src/users/user.model';
