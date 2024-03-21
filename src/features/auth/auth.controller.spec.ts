ac3d01dbc5f6468e33358c2cb118a120928e5611
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2024-03-21 22:55:17 +0100

    fix: update jest config in package-lock.json

---
+++ src/features/auth/auth.controller.spec.ts
@@ -0,0 +1,23 @@
+import { Test } from '@nestjs/testing';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+describe('AuthController', () => {
+  let controller: AuthController;
+  let service: AuthService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [AuthService],
+      imports: [Test],
+    }).compile();
+
+    controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
