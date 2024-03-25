11370d608c7cce0f99fdee9ce5ad366e8a811dc5
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2024-03-25 22:04:18 +0100

    feat: add dependencies and providers to AuthService spec, first test is passing

--- src/features/auth/auth.service.spec.ts
+++ src/features/auth/auth.service.spec.ts
@@ -1,12 +1,38 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { getModelToken } from '@nestjs/sequelize';
+import { Test, TestingModule } from '@nestjs/testing';
+import { User } from '../users/user.model';
+import { get } from 'http';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  const mockSequelizeUsers = {
+    findOne: jest.fn(),
+  };
+
+  const mockAuthService = {
+    SignUp: jest.fn(),
+    SignIn: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        UsersService,
+        JwtService,
+        {
+          provide: getModelToken(User),
+          useValue: mockSequelizeUsers,
+        },
+        {
+          provide: getModelToken(AuthService),
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
