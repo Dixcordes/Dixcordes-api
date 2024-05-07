75140005d631ff4b5f4ccbc434abc0976e5d9887
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2024-05-07 16:36:18 +0200

    fix: the violates unique constraint error in friend.model.ts and in friend-request.model.ts

--- src/features/friends-request/model/friend-request.model.ts
+++ src/features/friends-request/model/friend-request.model.ts
@@ -10,16 +10,18 @@ import { User } from '../../users/user.model';
 @Table({ tableName: 'friends-request' })
 export class FriendsRequest extends Model {
   @ForeignKey(() => User)
+  @Column({ allowNull: false, unique: false, field: 'from' })
   from: number;
 
   @ForeignKey(() => User)
+  @Column({ allowNull: false, unique: false, field: 'to' })
   to: number;
 
-  @BelongsTo(() => User, 'from')
-  user: User;
+  // @BelongsTo(() => User, 'from')
+  // user: User;
 
-  @BelongsTo(() => User, 'to')
-  friend: User;
+  // @BelongsTo(() => User, 'to')
+  // friend: User;
 
   @Column({ defaultValue: null })
   answer: boolean;
--- src/features/friends/models/friend.model.ts
+++ src/features/friends/models/friend.model.ts
@@ -1,17 +1,19 @@
-import { Table, Model, ForeignKey, BelongsTo } from 'sequelize-typescript';
+import { Table, Model, ForeignKey, Column } from 'sequelize-typescript';
 import { User } from '../../users/user.model';
 
 @Table({ tableName: 'friends' })
 export class Friends extends Model {
   @ForeignKey(() => User)
+  @Column({ allowNull: false, unique: false, field: 'user_id' })
   userId: number;
 
   @ForeignKey(() => User)
+  @Column({ allowNull: false, unique: false, field: 'target_id' })
   targetId: number;
 
-  @BelongsTo(() => User, 'userId')
-  user: User;
+  // @BelongsTo(() => User, 'userId')
+  // user: User;
 
-  @BelongsTo(() => User, 'targetId')
-  friend: User;
+  // @BelongsTo(() => User, 'targetId')
+  // friend: User;
 }
