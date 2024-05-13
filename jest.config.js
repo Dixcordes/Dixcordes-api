dc8cbeebc70107fcf2ef279c68b5a944f74e2999
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2024-05-13 11:56:35 +0200

    feat: add jest.config.js, update tsconfig and add friends service test, and update app controller, service and test

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '~/(.*)': '<rootDir>/src/$1',
+  },
+  modulePaths: ['<rootDir>'],
+  coverageDirectory: '../coverage',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+};
